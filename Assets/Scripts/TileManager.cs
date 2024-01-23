f52128d7afab44bc2e011f281be7acd419eea1ea
Chris <cvanden8@gmail.com>
2024-01-23 22:51:34 +0000

    chunking almost finished

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -25,13 +25,14 @@ public class TileManager : MonoBehaviour
     // private BoundsInt bounds2;
     // private BoundsInt bounds3;
     // private BoundsInt bounds4;
-    private TileBase[,] tiles1;
+    private TileBase[] tiles1;
     private TileBase[] flattenedTiles;
-    private TileBase[,] nullTiles;
+    private TileBase[] nullTiles;
     private TileBase[] flattenedNullTiles;
 
     [SerializeField]
     private TileBase testTile;
+    private int chunkSize = 10;
 
     // private TileBase[,] tiles2;
     // private TileBase[,] tiles3;
@@ -45,19 +46,23 @@ public class TileManager : MonoBehaviour
         // bounds2 = map2.cellBounds;
         // bounds3 = map3.cellBounds;
         // bounds4 = map4.cellBounds;
-        tiles1 = new TileBase[bounds1.size.x, bounds1.size.y];
-        nullTiles = new TileBase[bounds1.size.x, bounds1.size.y];
+        tiles1 = new TileBase[bounds1.size.x * bounds1.size.y];
+        nullTiles = new TileBase[bounds1.size.x * bounds1.size.y];
 
         // tiles2 = new TileBase[bounds2.size.x, bounds2.size.y];
         // tiles3 = new TileBase[bounds3.size.x, bounds3.size.y];
         // tiles4 = new TileBase[bounds4.size.x, bounds4.size.y];
+        int boundWidth = bounds1.xMax - bounds1.xMin;
+        int boundHeight = bounds1.yMax - bounds1.yMin;
+        Debug.Log(boundWidth);
         for (int y = bounds1.min.y; y < bounds1.max.y; y++)
         {
             for (int x = bounds1.min.x; x < bounds1.max.x; x++)
             {
                 Vector3Int pos = new Vector3Int(x, y, 0);
-                tiles1[x - bounds1.min.x, y - bounds1.min.y] = map1.GetTile(pos);
-                nullTiles[x - bounds1.min.x, y - bounds1.min.y] = null;
+                tiles1[x - bounds1.min.x + ((y - bounds1.min.y) * boundWidth)] = map1.GetTile(pos);
+                nullTiles[x - bounds1.min.x + ((y - bounds1.min.y) * boundWidth)] = null;
+                // nullTiles[x - bounds1.min.x, y - bounds1.min.y] = null;
             }
         }
         BoundsInt playerPos =
@@ -65,15 +70,16 @@ public class TileManager : MonoBehaviour
                 (int)Math.Floor(player.transform.position.x) - 5,
                 (int)Math.Floor(player.transform.position.y) - 5,
                 0,
-                10,
-                10,
+                chunkSize,
+                chunkSize,
                 1
             );
-        flattenedTiles = FlattenArray(tiles1);
-        flattenedNullTiles = FlattenArray(nullTiles);
-        map1.SetTilesBlock(bounds1, flattenedNullTiles);
-        map1.SetTilesBlock(bounds1, flattenedTiles);
-        // LoadChunk(map1, flattenedTiles, playerPos);
+        BoundsInt testPos = new(bounds1.xMin, bounds1.yMin - 5, 0, boundWidth, boundHeight, 1);
+        // flattenedTiles = FlattenArray(tiles1);
+        // flattenedNullTiles = FlattenArray(nullTiles);
+        map1.SetTilesBlock(bounds1, nullTiles);
+        // map1.SetTilesBlock(bounds1, tiles1);
+        LoadChunk(boundWidth, map1, tiles1, playerPos);
     }
 
     // Update is called once per frame
@@ -94,31 +100,52 @@ public class TileManager : MonoBehaviour
         return output;
     }
 
-    private void LoadChunk(Tilemap tilemap, TileBase[] tiles, BoundsInt pos)
+    private void LoadChunk(int width, Tilemap tilemap, TileBase[] tiles, BoundsInt pos)
     {
-        // Debug.Log(bounds1);
-        TileBase[] chunkData = new TileBase[100];
-        // int chunkSize = 10;
-        // for (int i = 0; i < 10; i++)
-        // {
-        //     for (int j = 0; j < 10; j++)
-        //     {
-        //         chunkData[i - 1 * j] = tiles[
-        //             Math.Abs(bounds1.xMax - bounds1.xMin) * Math.Abs(pos.y - bounds1.yMin) + i
-        //                 + Math.Abs(pos.x - bounds1.xMin)
-        //                 + j
-        //         ];
-        //     }
-        // }
+        TileBase[] chunkData = new TileBase[chunkSize * chunkSize];
         int index = 0;
-        for (int y = pos.y; y < pos.y + 10; y++)
+        for (int i = 0; i < chunkSize; i++)
         {
-            for (int x = pos.x; x < pos.x + 10; x++)
+            for (int j = 0; j < chunkSize; j++)
             {
-                int arrayIndex = y * (bounds1.xMax - bounds1.xMin + 1) + x;
-                chunkData[index++] = tiles[arrayIndex];
+                chunkData[index] = tiles[
+                    width * Math.Abs(pos.y + i - bounds1.yMin) + Math.Abs(pos.x - bounds1.xMin) + j
+                ];
+                index++;
             }
         }
         tilemap.SetTilesBlock(pos, chunkData);
     }
 }
+
+//big brain solution (in typescript)
+// type Grid<T> = {
+//   dim: {
+//     xMin: number;
+//     xMax: number;
+//     yMin: number;
+//     yMax: number;
+//   },
+//   cells: T[]
+// };
+
+
+// type Coord = {
+//   posX: number;
+//   posY: number;
+// }
+
+// const get_rekt = <T>(grid: Grid<T>, bottom_left: Coord, chunksize: number ) => {
+//   const stride = (grid.dim.xMax - grid.dim.xMin) + 1
+//   let src_index = bottom_left.posX + bottom_left.posY * stride;
+//   // allocate result array;
+//   const result = Array.from({length: chunksize * chunksize});
+//   let dst_index = 0;
+//   for (let y = 0; y < chunksize; y++) {
+//     for(let x = 0; x < chunksize; x++) {
+//       result[dst_index++] = grid.cells[src_index++];
+//     }
+//     src_index += stride - chunksize;
+//   }
+//   return result;
+// }
